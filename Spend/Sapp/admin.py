7a8d4df1e2866be3c7702b14fbb78ee0a3ef1838
hirwa cedric <hirwacedr12@gmail.com>
2025-01-16 08:56:49 +0200

    registered models

--- Spend/Sapp/admin.py
+++ Spend/Sapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from . models import Account, Transaction, Budget, Category
+
+admin.site.register(Account)
+admin.site.register(Transaction)
+admin.site.register(Budget)
+admin.site.register(Category)
