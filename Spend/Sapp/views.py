109d826b9213efff377b1ccfc5e7a26bda1197ec
hirwa cedric <hirwacedr12@gmail.com>
2025-01-18 12:30:24 +0200

    feat: add custom error handlers for 400, 403, 404, and 500 error pages

--- Spend/Sapp/views.py
+++ Spend/Sapp/views.py
@@ -17,6 +17,20 @@ from django.utils.timezone import localtime, now
 from datetime import timedelta
 
 
+# views.py
+def handler404(request, exception):
+    return render(request, 'errors/404.html', status=404)
+
+def handler403(request, exception):
+    return render(request, 'errors/403.html', status=403)
+
+def handler500(request):
+    return render(request, 'errors/500.html', status=500)
+
+def handler400(request, exception):
+    return render(request, 'errors/400.html', status=400)
+
+
 def signup_view(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
