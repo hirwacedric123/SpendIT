d413e63ff9c5572fc83ab6f7c9b716cedc0c070d
hirwa cedric <hirwacedr12@gmail.com>
2025-01-18 12:57:51 +0200

    feat: update error handlers to use simplified template paths and add 404 SVG image

--- Spend/Sapp/views.py
+++ Spend/Sapp/views.py
@@ -19,16 +19,16 @@ from datetime import timedelta
 
 # views.py
 def handler404(request, exception):
-    return render(request, 'errors/404.html', status=404)
+    return render(request, '404.html', status=404)
 
 def handler403(request, exception):
-    return render(request, 'errors/403.html', status=403)
+    return render(request, '403.html', status=403)
 
 def handler500(request):
-    return render(request, 'errors/500.html', status=500)
+    return render(request, '500.html', status=500)
 
 def handler400(request, exception):
-    return render(request, 'errors/400.html', status=400)
+    return render(request, '400.html', status=400)
 
 
 def signup_view(request):
